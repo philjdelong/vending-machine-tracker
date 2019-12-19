fc9aad80db33890cce4bd8e6192d824cbe5a12bc
Phil DeLong <philjdelong@gmail.com>
2019-12-19 09:43:24 -0700

    Update seeds and add text to view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+owner = Owner.create(  name: "Phil")
+
+machine = owner.machines.create(  location: "Denver")
+
+snack_1 = machine.snacks.create(  name: "Cheetos",
+                                  price: 1
+)
+
+snack_2 = machine.snacks.create(  name: "Fretos",
+                                  price: 2
+)
